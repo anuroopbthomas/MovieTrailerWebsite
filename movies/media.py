3b31a3ff5b31a8d13a78e6a372a295d813c0521d
Anuroop Thomas <anuroopbthomas@gmail.com>
2017-10-16 10:59:31 -0700

    formatting for PEP-8

--- movies/media.py
+++ movies/media.py
@@ -1,29 +1,42 @@
-#import of module webbrowser
+# import of module webbrowser
 import webbrowser
 
-#class Movie is being made here in media.py
+
+# class Movie is being made here in media.py
 class Movie():
-    #documentation of class
+
+    # documentation of class
     """ This class provides a way to store movie related information
 
         Attributes:
             title: the string movie title for any given instance
             storyline: the string storyline for any given isntance
-            poster_image_url: the string that contains the url for the image of the movie's poster_image_url
-            trailer_youtube_url: the string that contains the url for the YouTube link of movie trailer
+            poster_image_url: the string that contains the url
+                              for the image of the movie's poster_image_url
+            trailer_youtube_url: the string that contains the url
+                                 for the YouTube link of movie trailer
     """
 
-    #Class variables
+    # Class variables
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    #Initial constructor function that is called with a new instance is created
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        #Instance Variables
-        """Inits Movie with title, storyline, poster_image_url, and trailer_youtube_url        """
+
+    # Initial constructor function that
+    # is called with a new instance is created
+    def __init__(self, movie_title,
+                 movie_storyline, poster_image,
+                 trailer_youtube):
+        # Instance Variables
+        """Inits Movie with title, storyline,
+           poster_image_url, and trailer_youtube_url
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-    #Function that will open webbrowser with the movie's trailer
+
+    # Function that will open webbrowser with the movie's trailer
     def show_trailer(self):
-        """Performs the action of opening the webbrowser with the youtube trailer link"""
+        """Performs the action of opening
+           the webbrowser with the youtube trailer link
+        """
         webbrowser.open(self.trailer_youtube_url)
